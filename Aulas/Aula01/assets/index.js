9b19303b9148a75c6cbec46e5444ad3d088b4597
Al3ncar <alencar29htcss@gmail.com>
2023-10-31 22:55:01 -0300

    Aula 01

---
+++ Aulas/Aula01/assets/index.js
@@ -0,0 +1,18 @@
+// Logica de programação com Js
+
+// Lógica: sequencia de passos a serem executadasem ordem capaz de resolver um problema especificos
+// algoritimos seria uam sequencia de dados, sendo assim:
+
+// algoritimos: Sequencia de logica
+
+let carro = 'passando'
+let eu;
+
+
+if (carro === 'passando') {
+    eu = 'parado'
+} else {
+    eu = 'atravesar'
+}
+
+console.log(`Eu devo estar ${eu}`)
