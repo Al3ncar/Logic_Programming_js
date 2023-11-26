692ccbea48f1dd410c870fe5b155de706719b35c
Al3ncar <alencar29htcss@gmail.com>
2023-11-26 16:53:10 -0300

    Aula05: digite 2 numeros e mostre a media

--- Aula05/assets/index.js
+++ Aula05/assets/index.js
@@ -7,9 +7,15 @@ const firstN2 = Number(n2);
 console.log(typeof firstN1, firstN1);
 console.log(typeof firstN2, firstN2);
 
-// const medResult = firstN1 + firstN2 / 2; 
+// const medResult = firstN1 + firstN2 / 2;
 // presedencia de operadores
 
 const medResult = (firstN1 + firstN2) / 2;
 alert("seu resultado é: " + medResult);
+
+// Operadores aritmeticos basicos
+// ( + ) = Adição
+// ( - ) = Subtração
+// ( * ) = Multiplicação
+// ( / ) = Divisão
 
