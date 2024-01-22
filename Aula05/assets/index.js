5f3e2e01603583be49dbce7322c9697e80b3135d
Al3ncar <alencar29htcss@gmail.com>
2024-01-22 13:00:24 -0300

    Aula 05

--- Aula05/assets/index.js
+++ Aula05/assets/index.js
@@ -1,21 +1,84 @@
-const n1 = prompt("digite o primeiro numero");
-const firstN1 = Number(n1);
+// Mais operadores Aritmeticos
 
-const n2 = prompt("digite o primeiro numero");
-const firstN2 = Number(n2);
+// ( % ) = Modulo
+// ( ** ) = Exponenciação
+// ( ++ ) = Incremento
+// ( -- ) = Decremento
 
-console.log(typeof firstN1, firstN1);
-console.log(typeof firstN2, firstN2);
+// Modulo: Resto da divisão
 
-// const medResult = firstN1 + firstN2 / 2;
-// presedencia de operadores
+console.log(5 % 4); // 1
+console.log(2 % 2); // 0
+console.log(10 % 9); // 1
+console.log(6 % 2); // 0
+console.log(8 % 7); // 1
 
-const medResult = (firstN1 + firstN2) / 2;
-alert("seu resultado é: " + medResult);
+console.log(" "); // espaço console
 
-// Operadores aritmeticos basicos
-// ( + ) = Adição
-// ( - ) = Subtração
-// ( * ) = Multiplicação
-// ( / ) = Divisão
+// Exponenciação
 
+console.log(3 ** 2); // 9
+console.log(5 ** 3); // 125
+console.log(2 ** 3); // 8
+
+// Incremento e Decremento
+console.log(" "); // espaço console
+
+let n1 = 0;
+
+n1++;
+n1++;
+n1++;
+n1++;
+n1++;
+console.log(n1);
+
+n1--;
+n1--;
+n1--;
+n1--;
+n1--;
+console.log(n1);
+
+console.log(" "); // espaço console
+
+// Ordem do operador de incremento e decremento
+// colocar o nome da let atraz dos operadores pegamos o valor dela e adicionamos uma soma ou adição=
+// se nos fizermos ao contrario colocar os operadores primeiro e o valor da let depois
+// teramos uma alteração quase instantanea, exemplo:
+
+let n2 = 1;
+let n3 = 1;
+
+console.log(n2);
+console.log(n3);
+
+n2++;
+// n2: pega o numero
+// ++: adicionar +1
+console.log(n2); // 2
+
+++n3;
+// ++: adicionar +1
+// n3: pega o numero
+console.log(n3); // 2
+
+console.log(" "); // espaço console
+
+// Mesma coisa com o decremento
+
+let n4 = 1;
+let n5 = 1;
+
+console.log(n4);
+console.log(n5);
+
+n4--;
+// n4: pega o numero
+// --: diminui -1
+console.log(n4); // 2
+
+--n5;
+// --: diminui -1
+// n5: pega o numero
+console.log(n5); // 2
