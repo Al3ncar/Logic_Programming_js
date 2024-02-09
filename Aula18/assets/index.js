b1913030c10e77d9597ae43eaeea8c2ba8a77fe3
Al3ncar <alencar29htcss@gmail.com>
2024-02-09 16:51:38 -0300

    Aula 18

--- Aula18/assets/index.js
+++ Aula18/assets/index.js
@@ -1,27 +1,29 @@
-function Desafio() {
-  // Desafio
+// Desafio
 
-  // Pedir para o usuário um número e mostrar a
-  // desse numero de 1 a 1000
+// Pedir para o usuário um número e mostrar a
+// desse numero de 1 a 1000
 
-  const divContainer = document.querySelector(".box");
-  let msg = "";
+const divContaainer = document.querySelector(".box");
+let msg = "";
 
-  function NovaTabuada() {
-    alert("Nova Tabuada");
-  }
+const getResult = () => {
+  const textPrompt = prompt("Digite um numero de 0 a 100");
+  const numberResult = Number(textPrompt);
+  return numberResult;
+};
 
-  function LimparTabuada() {
-    alert("Limpar Tabuada");
-  }
-
-  NovaTabuada();
-  LimparTabuada();
+const novaTabuada = () => {
+  const promptResult = getResult();
+  limparTabuada()
 
   for (let i = 0; i <= 1000; i++) {
-    msg = `i: ${i}`;
+    msg += `<li>${promptResult} x ${i} = ${promptResult * i}</li>`;
   }
 
-  divContainer.innerHTML += msg;
-}
-Desafio();
+  divContaainer.innerHTML = msg;
+};
+
+const limparTabuada = () => {
+  msg = '';
+  divContaainer.innerHTML = msg;
+};
