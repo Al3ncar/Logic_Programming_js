88b135a79b4c04f59530fb5a85912852ffa9dd45
Al3ncar <alencar29htcss@gmail.com>
2024-03-06 12:13:53 -0300

    Aula 19

--- Aula19/assets/index.js
+++ Aula19/assets/index.js
@@ -46,3 +46,40 @@ function clear() {
   mgs = 0;
   divContainer.innerHTML = mgs;
 }
+
+// Codigo da aula
+
+var n1 = prompt("digite outro numero??");
+n1 = parseFloat(n1);
+
+escrever();
+
+function escrever() {
+  var output = document.querySelector(".result");
+  output.innerHTML = n1;
+}
+
+function add() {
+  var n = prompt("digite outro numero!!");
+  n = parseFloat(n);
+  n1 = n1 + n;
+  escrever();
+}
+function sub() {
+  var n = prompt("digite outro numero!!");
+  n = parseFloat(n);
+  n1 = n1 - n;
+  escrever();
+}
+function mult() {
+  var n = prompt("digite outro numero!!");
+  n = parseFloat(n);
+  n1 = n1 * n;
+  escrever();
+}
+function div() {
+  var n = prompt("digite outro numero!!");
+  n = parseFloat(n);
+  n1 = n1 / n;
+  escrever();
+}
